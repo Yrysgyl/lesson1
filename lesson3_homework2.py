7691eb89f48779d4126d3b1fa40cb3c5781a120d
Yrysgyl <164658092+Yrysgyl@users.noreply.github.com>
2024-03-26 20:25:08 +0600

    second

---
+++ lesson3_homework2.py
@@ -0,0 +1,148 @@
+# num = int(input(''))
+# if num % 2 == 0:
+#     print('четный')
+# else:
+#     print('нечетный')
+#
+# number1 = int(input(''))
+# number2 = int(input(''))
+#
+# if number1 > number2:
+#     print(number1)
+# elif number1 == number2
+#     print('==')
+# else:
+#     print(number2)
+#
+#
+# if number2 % number1 = 0 :
+#     print('yes')
+#
+# elif number1 % 2 = 0:
+#     print('yes')
+# else:
+#     print('not ')
+
+# a = int(input(''))
+# b = int(input(''))
+# c = int(input(''))
+#
+# if a == b and b == c:
+#     print('равносторонний')
+# elif a == b and b != c or b == c and c != a or c == a and a != b:
+#     print('равнобедренным')
+# else:
+   # print('разностороонний')
+
+# time = float(input('')):
+# if 0 <= time <= 8:
+#     print('night')
+# elif 8 < time <= 10:
+#     print('morning')
+# elif 11 <= time <= 16:
+#     print('afternoon')
+# elif 16 < time < 0:
+#     print('evening')
+# else:
+#     print('error')
+
+# str_ch = input('')
+# simvol = input('')
+#
+# if simvol in str_ch:
+#     print('yes')
+# else:
+#     print('not')
+
+# year = int(input(''))
+# if year % 4 == 0:
+#     print('високосный')
+# elif year % 100 == 0 and year % 400 == 0:
+#     print('високосный')
+# else:
+#     print('не високосный')
+
+# num1 = int(input(''))
+# num2 = int(input(''))
+# num3 = int(input(''))
+#
+# if num1 < num2 and num1 < num3:
+#     print(num1)
+# elif num2 < num1 and num2 < num3:
+#     print(num2)
+# elif num3 < num1 and num3 < num2:
+#     print(num3)
+# else:
+#     print('нельза давать 2 одинаковых числа')
+
+# number = int(input(''))
+# if number % number == 0 and number % 1 == 0 and number % 2 != 0:
+#     print('простое число')
+# else:
+#     print('непростое число')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task_list.py
@@ -0,0 +1,157 @@
+# a = [2, 4, 6, 8, 10]
+# for num in a:
+#     print(num)
+import random
+
+# five_num=[3, 0, 3, 8, 2]
+# print(five_num[2:4])
+
+# delete = list(input(''))
+# delete.remove('а')
+# delete.remove('е')
+# delete.remove('о')
+# print(delete)
+
+# a=[1, 9, 3, 5, 7]
+# b=[1, 2, 3, 4, 5]
+# for num in a:
+#     if num in b:
+#         b.remove(num)
+#         print(b)
+
+# import random
+# number = random.random()*100
+# print(number)
+
+# num=random.randint(4,15)
+# print(num)
+#
+# num1=random.randrange(10)
+# print(num1)
+#
+# num2=random.randrange(2,47)
+# print(num2)
+#
+# num3=random.randrange(5,15,3)
+# print(num3)
+
+# numbers=[2, 4, 6, 8, 3, 5, 1]
+# random.shuffle(numbers)
+# print(numbers)
+
+# random_num=random.choice(numbers)
+# print(random_num)
+
+# empt_y = []
+# for num in range(7):
+#     empt_y.append(random.randint(2,25))
+# print(empt_y)
+
+# empty_2 = []
+# for x in range(10):
+#    # empty_2.append(random.random())
+#     #empty_2.append(random.randint(15, 31))
+#     empty_2.append(random.randrange(25))
+# print(empty_2)
+# print(max(empty_2))
+# print(min(empty_2))
+# print(sum(empty_2))
+# print(sum(empty_2)//len(empty_2))
+
+# empty_3=[]
+# maxi=0
+#
+# for x in range(10):
+#     empty_3.append(random.randint(4, 15))
+# print(empty_3)
+# a=empty_3.copy()
+# for x in range(10):
+#     if a[x] > a [x-1]:
+#         if a [x] > a [x+1]:
+#             maxi = a [x]
+# print(a)
+# print(maxi)
+
+# x= []
+#
+# for y in range(7):
+#      x.append(random.randint(2,10))
+#      z=x.copy()
+#      print(z)
+# for y in z:
+#      #if y in z:
+#      e=z.count(y)
+#      print(e, end=' ')
+
+# random_list = []
+# for x in range(8):
+#     random_list.append(random.randint(15,25))
+# print(random_list)
+# random_list.remove(max(random_list))
+# print(random_list)
+# print(max(random_list))
+
+# list_random = []
+# for x in range(5):
+#     list_random.append(random.randint(5, 25))
+# print(list_random)
+# print(sum(list_random))
+
+
+# def a=(b, box):
+#     for num in b:
+#         if num = box:
+#             print(1)
+#             return
+#         else:
+#             print(2)
+#
+# lst=[1,2,3]
+# ex_num(lst, 0)
+# ex_num(lst, 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
