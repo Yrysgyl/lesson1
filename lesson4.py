1313ec3e7429ee8d3141bc8e21b86902e6961b95
Yrysgyl <164658092+Yrysgyl@users.noreply.github.com>
2024-03-28 06:09:18 +0600

    third

---
+++ lesson4.py
@@ -0,0 +1,64 @@
+# age = int(input(''))
+# if  12 <= age <=18:
+#     print('teeneger')
+# elif 18 < age <= 25:
+#     print('student')
+
+# num = int(input(''))
+# if num % 2 == 0:
+#     print(num*2)
+# else:
+#     print('not')
+
+# word = input('')
+# if len(word) > 5:
+#     print('long')
+# else:
+#     print('short')
+
+# n = int(input(''))
+# for x in range(n+1):
+#     print(x)
+
+# #задача 2
+# k = int(input(''))
+# m = int(input(''))
+# for x in range(k, m+1):
+#     print(x)
+#
+# #задача 3
+# total = 0
+# for x in range(k, m+1):
+#    # if x % 2 == 0:
+#         total += x
+# print(total)
+
+
+# total = 0
+# n = int(input(''))
+# for x in range(n):
+#     total += n/10
+# print(n + total)
+
+
+# total = 0
+# n = int(input(''))
+# for x in range(n):
+#     total += 1/n
+# print(n + total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
