99dcde0f1cf6b038f6544d9146c04a8a73ae00a2
Yrysgyl <164658092+Yrysgyl@users.noreply.github.com>
2024-04-24 19:53:24 +0600

    classy

---
+++ class_homework.py
@@ -0,0 +1,45 @@
+# class Student:
+#     def __init__(self,name, age, course):
+#         self.name = name
+#         self. age = age
+#         self. course = course
+#
+#     def info(self):
+#         print(f'Name: {self.name} Age: {self.age} course: {self.course}')
+#
+#
+# tom = Student('tom', 20, 4)
+#
+# tom.info()
+
+
+
+
+# class Book:
+#     def __init__(self, title, author, year):
+#         self.title = title
+#         self.author = author
+#         self.year = year
+#
+#     def info_book(self):
+#         print(f'Название: {self.title} ,Автор: {self.author}, Год выпуска: {self.year}')
+#
+#
+# math = Book('math', 'Ibraimov', 2010)
+#
+# math.info_book()
+
+
+# class Tovar:
+#     def __init__(self, name, price, quantity):
+#         self.name = name
+#         self.price = price
+#         self.quantity = quantity
+#
+#     def info_tovar(self):
+#         print(f'название: {self.name}, цена: {self.price}, количество на складе: {self.quantity}')
+#
+#
+# tea = Tovar('Greentea', 200, 15)
+#
+# tea.info_tovar()
